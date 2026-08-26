797e99982a38bcdab53fa1b64c3892a111620b4b
Dezzles <daniel@dezzles.com>
2026-08-26 20:45:02 +0200

    Clean up error messages in AuthMiddleware

--- rq-server/middleware/validate-user.go
+++ rq-server/middleware/validate-user.go
@@ -24,7 +24,7 @@ func NewAuthMiddleware(config *model.AppConfig) *AuthMiddleware {
 func (am *AuthMiddleware) ValidateUser(c *gin.Context) {
 	authHeader := c.GetHeader("Authorization")
 	if authHeader == "" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "authorization header is required"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
 		c.Abort()
 		return
 	}
